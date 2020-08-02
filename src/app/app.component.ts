8af0453c72d558afc76e73340e431458789cf93d
Thành Nhân <thanhnhan.toantin@gmail.com>
2020-08-02 20:08:46 +0700

    change status bar color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,8 +20,9 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    this.statusBar.overlaysWebView(false);
+    this.statusBar.backgroundColorByHexString('#de600f');
   }
 }
